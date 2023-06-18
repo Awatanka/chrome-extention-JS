3c4aaabe08dddefcdf39d4afe488e6104f2bdef1
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2023-06-18 14:41:56 -0700

    Add basic input functionality and style

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const saveBtn = document.getElementById("input-btn");
 const link = "www.awesomelead.com";
+const ulEl = document.getElementById("ul-el");
 
 saveBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+  inputEl.value = "";
+  renderLeads();
 });
 
-for (let i = 0; i < myLeads.length; i++) {
-  console.log(myLeads[i]);
+function renderLeads() {
+  let listItems = "";
+
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `<li><a target="_blank" href="${myLeads[i]}">${myLeads[i]}</a></li>`;
+  }
+
+  ulEl.innerHTML = listItems;
 }
