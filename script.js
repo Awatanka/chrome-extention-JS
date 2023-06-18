3b1ee7abcf024c11f65e1048fe5e9a84f965a9c7
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2023-06-18 16:19:21 -0700

    Add more functionality

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ const saveBtn = document.getElementById("input-btn");
 const link = "www.awesomelead.com";
 const ulEl = document.getElementById("ul-el");
 
+localStorage.clear();
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
+console.log(myLeads);
+
 saveBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   renderLeads();
+  console.log(localStorage.getItem("myLeads"));
 });
 
 function renderLeads() {
